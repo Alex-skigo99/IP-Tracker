68449ca939a3218953b01b7a35d8119e66959fed
Alexander BOLSHUKHIN <skigo99@gmail.com>
2024-10-03 16:03:14 +0300

    Init vite-react-jest app

---
+++ client/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    testEnvironment: 'jsdom',
+    transform: {
+      '^.+\\.jsx?$': 'babel-jest',
+    },
+    setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+    moduleNameMapper: {
+      '\\.(css|scss)$': 'identity-obj-proxy', // Mock CSS imports
+    },
+  };
---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>IP Tracker</h1>
+    </>
+  )
+}
+
+export default App
